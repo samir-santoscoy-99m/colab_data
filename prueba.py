c5ce2d8d0ed5200f076588d07a3c60f84fab0925
samir-santoscoy-99m <samir.santoscoy@99minutos.com>
2025-02-26 21:40:49 -0600

    V4

--- prueba.py
+++ prueba.py
@@ -1,8 +1,8 @@
 def sumar(a, b):
     return a + b
 
-def restar(a,b, c):
-    return a - b - c
+def restar(a,b):
+    return a - b
 
 if __name__ == "__main__":
     print("Este es un script de prueba.")
